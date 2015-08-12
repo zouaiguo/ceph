4216761302f953b76c4f49d137fcd4ee8f9e7173
Sage Weil <sage@redhat.com>
2015-08-12 14:19:08 -0400

    fix up test script

--- qa/workunits/fs/test_path_read-only.sh
+++ qa/workunits/fs/test_path_read-only.sh
@@ -2,14 +2,15 @@
 
 mkdir -p mnt.admin mnt.foo
 
-./ceph-fuse -n client.admin mnt.admin
-mkdir mnt.admin/bar
-touch mnt.admin/out mnt.admin/bar/out
-echo READ TEST PASSED >> mnt.admin/out
-echo READ TEST IN TREE PASSED >>mnt.admin/bar/out
+sudo ./ceph-fuse -n client.admin mnt.admin
+sudo mkdir -p mnt.admin/bar
+sudo chmod 777 mnt.admin/bar
+sudo touch mnt.admin/bar/out
+sudo chmod 777 mnt.admin/bar/out
+echo READ TEST PASSED >>mnt.admin/bar/out
 
 ./ceph auth get-or-create client.foo mon 'allow r' mds 'allow r' osd 'allow rwx' >> keyring
-./ceph-fuse -n client.foo mnt.foo
+sudo ./ceph-fuse -n client.foo mnt.foo
 
 cleanup()
 {
@@ -30,10 +31,11 @@ expect_false()
 }
 
 # write operations are not allowed
-expect_false mkdir mnt.foo/newdir
-expect_false touch newfile
+expect_false mkdir mnt.foo/bar/newdir
 expect_false touch mnt.foo/bar/newfile
 
 # read-only operations are allowed
-cat mnt.foo/out
+ls mnt.foo/bar
 cat mnt.foo/bar/out
+
+echo OK
