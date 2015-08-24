3bb7323399fb1f56209fa787b46decdc9dede81d
Ivo Jimenez <ivo.jimenez@gmail.com>
2015-08-24 15:12:28 -0700

    Adds truncate-for-readonly test to direct_io workunit

---
+++ qa/workunits/direct_io/test_short_dio_read_trunc.c
@@ -0,0 +1,69 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+  char buf[409600];
+  ssize_t r;
+  int err;
+  int fd = open("othershortfile", O_WRONLY|O_CREAT, 0644);
+
+  if (fd < 0) {
+          err = errno;
+          printf("error: open() failed with: %d (%s)\n", err, strerror(err));
+          exit(err);
+  }
+
+  printf("writing first 3 bytes of file\n");
+  r = write(fd, "foo", 3);
+  if (r == -1) {
+          err = errno;
+          printf("error: write() failed with: %d (%s)\n", err, strerror(err));
+          close(fd);
+          exit(err);
+  }
+
+  fsync(fd);
+  close(fd);
+
+  printf("reading O_DIRECT\n");
+  fd = open("othershortfile", O_RDONLY|O_DIRECT);
+  if (fd < 0) {
+          err = errno;
+          printf("error: open() failed with: %d (%s)\n", err, strerror(err));
+          exit(err);
+  }
+
+  printf("truncate file to 10000 bytes\n");
+  r = ftruncate(fd, 10000);
+  if (r == -1) {
+          err = errno;
+          printf("error: ftruncate() failed with: %d (%s)\n", err, strerror(err));
+          close(fd);
+          exit(err);
+  }
+  fsync(fd);
+  close(fd);
+
+  printf("reading O_DIRECT\n");
+  fd = open("othershortfile", O_RDONLY|O_DIRECT);
+  if (fd < 0) {
+          err = errno;
+          printf("error: open() failed with: %d (%s)\n", err, strerror(err));
+          exit(err);
+  }
+
+  r = read(fd, buf, sizeof(buf));
+  close(fd);
+
+  printf("got %d\n", (int)r);
+  if (r != 10000)
+          return 1;
+  return 0;
+}
