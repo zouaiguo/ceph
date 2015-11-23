a3020fda1cafa709b5ff28be220ea1057473f358
Yan, Zheng <zyan@redhat.com>
2015-11-23 21:57:24 +0800

    mds: repair fragstat/rstat errors in inode
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/mds/MDCache.h
+++ src/mds/MDCache.h
@@ -1143,6 +1143,7 @@ protected:
    * long time)
    */
   void enqueue_scrub_work(MDRequestRef& mdr);
+  void repair_inode_stats_work(MDRequestRef& mdr);
   void repair_dirfrag_stats_work(MDRequestRef& mdr);
   friend class C_MDC_RepairDirfragStats;
 public:
@@ -1153,6 +1154,7 @@ public:
    */
   void enqueue_scrub(const string& path, const std::string &tag,
                      Formatter *f, Context *fin);
+  void repair_inode_stats(CInode *diri, Context *fin);
   void repair_dirfrag_stats(CDir *dir, Context *fin);
 };
 
