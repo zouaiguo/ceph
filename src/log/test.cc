b53cce2c674707b42ffe1ad166ebfa30194c509d
Sage Weil <sage@redhat.com>
2015-11-29 09:20:16 -0500

    Revert "Merge branch 'wip-log-alloc-predictor' of git://github.com/aclamk/ceph into wip-sage-testing"
    This reverts commit 00609ce31e654726934ca2a5b233f3469d768f1a, reversing
    changes made to b6c6d241fa68eeeb07d22fea77f0788a674e9ddb.

--- src/log/test.cc
+++ src/log/test.cc
@@ -160,10 +160,9 @@ TEST(Log, ManyGatherLogPrebufOverflow)
     int l = 10;
     if (subs.should_gather(1, l)) {
       Entry *e = new Entry(ceph_clock_now(NULL), pthread_self(), l, 1);
-      PrebufferedStreambuf psb(e->m_static_buf, sizeof(e->m_static_buf));
+      PrebufferedStreambuf psb(e->m_static_buf, 20);
       ostream oss(&psb);
-      oss << "this i a long stream asdf asdf asdf asdf asdf asdf asdf asdf asdf as fd"
-          << std::string(sizeof(e->m_static_buf) * 2, '-') ;
+      oss << "this i a long stream asdf asdf asdf asdf asdf asdf asdf asdf asdf as fd";
       //e->m_str = oss.str();
       log.submit_entry(e);
     }
