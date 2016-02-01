c45e8ce0b7c39cfd78afb0f00d6b4bebb7b24522
Sage Weil <sage@redhat.com>
2016-02-01 10:50:44 -0500

    osd: probabilistic promotion throttling
    Global tunables osd_tier_promote_max_{objects,bytes}_sec adjust a
    probability that any given promotion we might otherwise do will be done
    or rejected.  Based on the observed rate of promotion, and the target/max,
    we will adjust the probability up for down.
    
    This avoids the complexity of a throttle queue and gets us a reasonably
    accurate throttle.  What it doesn't yet do is cope with thundering herds of
    promotions.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -2279,12 +2279,11 @@ bool ReplicatedPG::maybe_promote(ObjectContextRef obc,
 
   switch (recency) {
   case 0:
-    promote_object(obc, missing_oid, oloc, promote_op, promote_obc);
     break;
   case 1:
     // Check if in the current hit set
     if (in_hit_set) {
-      promote_object(obc, missing_oid, oloc, promote_op, promote_obc);
+      break;
     } else {
       // not promoting
       return false;
@@ -2310,15 +2309,18 @@ bool ReplicatedPG::maybe_promote(ObjectContextRef obc,
 	}
       }
       if (count >= recency) {
-	promote_object(obc, missing_oid, oloc, promote_op, promote_obc);
-      } else {
-	// not promoting
-	return false;
+	break;
       }
+      return false;	// not promoting
     }
     break;
   }
 
+  if (osd->promote_throttle()) {
+    dout(10) << __func__ << " promote throttled" << dendl;
+    return false;
+  }
+  promote_object(obc, missing_oid, oloc, promote_op, promote_obc);
   return true;
 }
 
@@ -7467,6 +7469,8 @@ void ReplicatedPG::finish_promote(int r, CopyResults *results,
     }
     return;
   }
+
+  osd->promote_finish(results->object_size);
 
   RepGather *repop = simple_repop_create(obc);
   OpContext *tctx = repop->ctx;
